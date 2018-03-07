e46a79b927b4a4b4a6b77ddf15bf34bac41369e3
Yusuke Wada <wada.yusuke@classmethod.jp>
2018-03-07 10:07:21 +0900

    fix code

--- test/functions/heroes/test_utils.py
+++ test/functions/heroes/test_utils.py
@@ -2,11 +2,13 @@
 
 import pytest
 import datetime
+import decimal
 from src.functions.heroes.utils import *
+
 
 def test_epoc_by_second_precision():
     tstr = '2012-12-29 13:49:37'
     tdatetime = datetime.strptime(tstr, '%Y-%m-%d %H:%M:%S')
     sut = epoc_by_second_precision(tdatetime)
 
-    assert sut == 1356756577
+    assert sut == decimal.Decimal('1356756577')
